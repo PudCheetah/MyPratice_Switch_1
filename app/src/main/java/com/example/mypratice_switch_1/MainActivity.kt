0fc3294cb31fb98e79718fe72964182e061ec87d
PudCheetah <wer212131@gmail.com>
2024-06-06 11:04:42 +0800

    新增了監聽器

--- app/src/main/java/com/example/mypratice_switch_1/MainActivity.kt
+++ app/src/main/java/com/example/mypratice_switch_1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mypratice_switch_1
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,5 +15,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        Toast.makeText(this, "$binding:{binding}", Toast.LENGTH_SHORT).show()
+
+        binding.MySwitch1.setOnCheckedChangeListener { buttonview, isChecked ->
+            if (isChecked) {
+                Toast.makeText(this, "開關${buttonview}${isChecked}", Toast.LENGTH_SHORT).show()
+                println("-------------------")
+                println("switch On")
+                println("-------------------")
+            } else {
+                Toast.makeText(this, "開關${buttonview}${isChecked}", Toast.LENGTH_SHORT).show()
+                println("-------------------")
+                println("switch OFF")
+                println("-------------------")
+            }
+        }
+    }
+
+    fun toastHere(buttonview: View, isCheck: Boolean){
+        Toast.makeText(this, "開關${buttonview}${isCheck}", Toast.LENGTH_SHORT).show()
     }
 }
